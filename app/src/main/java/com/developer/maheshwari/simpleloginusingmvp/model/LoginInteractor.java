a711ce5e9ce141f8443b48bc7a51fce43270ccb0
maheshwari-reddy <maheshwari.reddy92@gmail.com>
2017-08-04 22:27:25 +0530

    initial commit

--- app/src/main/java/com/developer/maheshwari/simpleloginusingmvp/model/LoginInteractor.java
+++ app/src/main/java/com/developer/maheshwari/simpleloginusingmvp/model/LoginInteractor.java
@@ -2,6 +2,8 @@ package com.developer.maheshwari.simpleloginusingmvp.model;
 
 import android.text.TextUtils;
 
+import com.developer.maheshwari.simpleloginusingmvp.utility.Utility;
+
 class LoginInteractor implements ILoginInteractor {
 
 
@@ -14,6 +16,14 @@ class LoginInteractor implements ILoginInteractor {
         }
         if (TextUtils.isEmpty(password)) {
             listener.loginFailed("Please enter password");
+            return;
+        }
+        if (!TextUtils.isEmpty(email) && !Utility.isValidEmail(email)) {
+            listener.loginFailed("Please enter valid email address");
+            return;
+        }
+        if (!TextUtils.isEmpty(password) && password.length() < 6) {
+            listener.loginFailed("Password length must be at least 6 characters");
             return;
         } else {
             listener.loginSuccess();
