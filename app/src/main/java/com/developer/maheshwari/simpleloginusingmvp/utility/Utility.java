8db4330f6e77552f50afc601298e5cd788617df0
maheshwari-reddy <maheshwari.reddy92@gmail.com>
2017-08-04 22:29:59 +0530

    initial commit

--- app/src/main/java/com/developer/maheshwari/simpleloginusingmvp/utility/Utility.java
+++ app/src/main/java/com/developer/maheshwari/simpleloginusingmvp/utility/Utility.java
@@ -5,7 +5,7 @@ import android.text.TextUtils;
 
 public class Utility {
 
-    public final static boolean isValidEmail(CharSequence email) {
+    public static boolean isValidEmail(CharSequence email) {
         return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
